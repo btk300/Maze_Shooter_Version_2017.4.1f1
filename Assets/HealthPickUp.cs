3220f401defb9336f9bf5789ab3a09d7459aead9
btk300 <36508646+btk300@users.noreply.github.com>
2018-05-03 12:02:59 +0100

    heath and trap

---
+++ Assets/HealthPickUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickUp : Interacable {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<PlayerHealth>().IncreaseHealth(30);
+        Interact();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+    }
+}
---
+++ Assets/Scripts/TrapDamage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDamage : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<PlayerHealth>().TakeDamage(100);
+    }
+
+
+}
